d7fc1c44dc48c284783b553f9c58bc3fa06afbe3
Victor Pontes <victorpontes2013@gmail.com>
2024-03-30 17:53:25 -0700

    Make it resizable in parent container

--- src/components/MinesweeperGame.tsx
+++ src/components/MinesweeperGame.tsx
@@ -1,33 +1,32 @@
 import { useEffect } from "react";
 import { Minesweeper, MinesweeperProps } from "../utils/Minesweeper"
 import { Tile } from "./Tile"
+import { debounce } from "lodash"
 
 export default function MinesweeperGame(props:MinesweeperProps) {
     const board = new Minesweeper(props).board;
     const boardWidth = board[0].length;
     const boardHeight = board.length;
 
-    const setContainerSquareDimensions = () => {
-        const container = document.getElementById('container')!;
-        const parent = container.parentElement;
-        if (parent) {
-            if (parent.clientHeight < parent.clientWidth) {
-                container.style.height = "100%";
-                container.style.width = container.clientHeight + "px";
-            } else {
-                container.style.height = container.clientWidth + "px";
-                container.style.width = "100%";
-            }
-        }
-    }
-
     useEffect(() => {
-        setContainerSquareDimensions();
-        window.addEventListener('resize', setContainerSquareDimensions);
+        const container = document.getElementById('container')!;
+        const parent = container.parentElement!;
+    
+        const resizeObserver = new ResizeObserver(debounce((entries) => {
+            for (let entry of entries) {
+                const { width, height } = entry.contentRect;
+                const size = Math.round(Math.min(width, height));
+                container.style.width = size + "px";
+                container.style.height = size + "px";
+            }
+        }));
+    
+        resizeObserver.observe(parent);
+    
         return () => {
-            window.removeEventListener('resize', setContainerSquareDimensions);
+            resizeObserver.unobserve(parent);
         };
-    }, []); // Empty dependency array to only add and remove the event listener once
+    }, []);
 
     return (
         <div id="container"></div>
