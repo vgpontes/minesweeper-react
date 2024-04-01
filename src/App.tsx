075982dec7e81ffb8ec44e14d843666062260189
Victor Pontes <victorpontes2013@gmail.com>
2024-03-31 21:04:22 -0700

    try to fix viewport bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import MinesweeperGame from './components/MinesweeperGame';
 
 function App() {
+  window.addEventListener('DOMContentLoaded', function() {
+    function adjustHeight() {
+        var windowHeight = window.innerHeight;
+        document.getElementById('main')!.style.height = windowHeight + 'px';
+    }
+
+    adjustHeight();
+
+    window.addEventListener('resize', function() {
+        adjustHeight();
+    });
+});
+  
   return (
-    <div style={{ width: "100vw", height: "100vh", backgroundColor: "green", display: "flex", alignItems: "center", justifyContent: "center"}}>
+    <div id="main" style={{ height: "100vh", width:"100vw", backgroundColor: "green", display: "flex", alignItems: "center", justifyContent: "center", overflow: "auto"}}>
       <MinesweeperGame numMines={10} boardHeight={9} boardWidth={9}/>
     </div>
   );
